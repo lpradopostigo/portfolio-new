5639fa1360bf085481429074487b632421396766
Luis Prado <lpradopostigo@gmail.com>
2022-01-21 21:08:11 -0500

    added contact info, new assets, deleted unused assets

--- source/components/Hero/Hero.jsx
+++ source/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import bg from "../../../assets/scattered-forcefields.svg";
+import background from "../../../assets/scattered-forcefields.svg";
+import github from "../../../assets/github.svg";
+import mail from "../../../assets/mail.svg";
 
 export default function Hero() {
   return (
@@ -8,13 +10,22 @@ export default function Hero() {
         <span className="text-8xl font-medium">Hi,</span>
         <span className="text-6xl">I'm Luis Prado,</span>
         <span className="text-4xl font-light">Front End Developer</span>
+
+        <div className="flex gap-6 items-center mt-8  w-max rounded-lg p-2 border-2">
+          <a href="mailto: lpradopostigo@gmail.com">
+            <img className="h-10 w-10" src={mail} alt="mail" />
+          </a>
+
+          <a href="https://github.com/lpradopostigo">
+            <img className="h-10 w-10" src={github} alt="github" />
+          </a>
+        </div>
       </div>
 
       <img
-        src={bg}
+        src={background}
         alt="background"
-        className="h-screen absolute object-cover top-0"
-        style={{ zIndex: -1 }}
+        className="h-screen absolute object-cover top-0 z-[-1]"
       />
     </section>
   );
